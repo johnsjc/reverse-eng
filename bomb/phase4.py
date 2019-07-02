c5d82a4dca27f281c086e31e482ad1c7ace3a326
Jc <johnsjc@gmail.com>
2019-07-01 21:21:33 -0600

    Update phase4.py

--- bomb/phase4.py
+++ bomb/phase4.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # Reverse engineering phase 4
 
+import sys
+
 def explode_bomb():
     print("\nBOOM!!!\nThe bomb exploded.")
+    sys.exit(16)
   
 def fib(x):
     if x <= 1: return 1
     else: return fib(x - 2) + fib(x - 1)
 
 def phase_4(answer):
+    # answer is a string %d
     try:
         answer = int(answer)
         if fib(answer) != 55:
@@ -21,7 +25,7 @@ def phase_4(answer):
 def solve():
     for x in range(1, 12):
       if fib(x) == 55:
-          return str(x) # the bomb expects a string
+          return str(x)
     return None
   
 phase_4(solve())
