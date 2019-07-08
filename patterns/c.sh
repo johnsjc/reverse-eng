d7006810ada4d907746764382a88b0da7f852d8c
Jc <johnsjc@gmail.com>
2019-07-08 07:50:33 -0600

    Update c.sh

--- patterns/c.sh
+++ patterns/c.sh
@@ -1,8 +1,15 @@
+# Script to compile a C program to multiple architectures
+# for the purposes of looking at differences in the assembly.
+# Stack canaries and PIE have been removed for learning purposes.
+# GCC supported. Will add clang, etc. later
+
 compile_gcc() {
+    # Compile for x86
     gcc -o x86 -mpreferred-stack-boundary=2 -fno-stack-protector -fno-pie -no-pie $1 -m32
-    gcc -o x64 -fno-stack-protector -fno-pie -no-pie $1
-    # with optimizations
     gcc -o x86o -mpreferred-stack-boundary=2 -fno-stack-protector -fno-pie -no-pie $1 -m32 -O4
+    
+    # Copmile for x64
+    gcc -o x64 -fno-stack-protector -fno-pie -no-pie $1    
     gcc -o x64o -fno-stack-protector -fno-pie -no-pie $1 -O4
 }
 
