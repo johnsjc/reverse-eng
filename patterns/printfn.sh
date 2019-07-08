64ad64a530158dba678d1d45d8cab62314037a2b
Jc <johnsjc@gmail.com>
2019-07-08 07:39:38 -0600

    update printfn.sh

--- patterns/printfn.sh
+++ patterns/printfn.sh
@@ -1,4 +1,6 @@
 echo "<${1%%.*} $2>:" # <x86 main>:
 
 # 
-cut -f3 $1 | awk -v pattern="$2>:" '$0 ~ pattern' RS= | awk '1;/^ret/{exit}' | sed 1d
+cut -f3 $1 \
+| awk -v pattern="$2>:" '$0 ~ pattern' RS= \
+| awk '1;/^ret/{exit}' | sed 1d
