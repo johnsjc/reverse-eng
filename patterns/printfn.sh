0eea202606ee78257555e89c16fc98063b563b92
Jc <johnsjc@gmail.com>
2019-07-08 07:47:26 -0600

    Update printfn.sh

--- patterns/printfn.sh
+++ patterns/printfn.sh
@@ -1,8 +1,16 @@
 # Output a first line like <x86 main>: 
 echo "<${1%%.*} $2>:"
 
+# only interested in the instructions
+# read all lines from function_name>: until blank line
+# remove all lines after ret (useless nops, garbage instructions, etc) 
+# remove the first line to replace with our echo 
 cut -f3 $1 \
-
 | awk -v pattern="$2>:" '$0 ~ pattern' RS= \
 | awk '1;/^ret/{exit}' \
 | sed 1d 
+
+#
+#
+#
+#
