a1746c857dcdd1c3968219cbf9f0d15d44c76cbd
Jc <johnsjc@gmail.com>
2019-07-08 07:48:18 -0600

    Update printfn.sh

--- patterns/printfn.sh
+++ patterns/printfn.sh
@@ -10,7 +10,17 @@ cut -f3 $1 \
 | awk '1;/^ret/{exit}' \
 | sed 1d 
 
+# Example:
 #
+# $ ../printfn.sh x86.asm main
 #
-#
+# <x86 main>:
+# push   ebp
+# mov    ebp,esp
+# push   0x80484c0
+# call   80482e0 <puts@plt>
+# add    esp,0x4
+# mov    eax,0x0
+# leave
+# ret
 #
