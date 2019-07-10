251559d24b21cf3ba347e1907095daa298a60079
Jc <johnsjc@gmail.com>
2019-07-10 07:26:55 -0600

    Update bubble.s

--- mips/bubble.s
+++ mips/bubble.s
@@ -19,19 +19,19 @@
 
             .text
 main:
+            li      $t7, 1                          # sorted = false
+            
+get_numbers:
             li      $t0, 0                          # i = 0
             la      $t1, numbers                    # $t1 points to numbers arr
-            li      $t7, 1                          # sorted = false
-
-get_numbers:
+get_numbers_loop:
             beq     $t0, 20, end_get_numbers        # if i == 20, we're done
             li      $v0, 5                          # otherwise call read_int
             syscall
             sw      $v0, ($t1)                      # store the int at memory address $t1
             addu    $t1, $t1, 4                     # increment $t1 pointer by 4 bytes
             add     $t0, $t0, 1                     # i++
-            b       get_numbers 
-
+            b       get_numbers_loop
 end_get_numbers:
             b       print_numbers
 
@@ -39,7 +39,6 @@ end_get_numbers:
 print_numbers:                                      
             li      $t0, 0                          # i = 0
             la      $t1, numbers                    # $t1 points to numbers arr
-
 print_numbers_loop:
             beq     $t0, 20, end_print_numbers      # if i == 20, we're done
             lb      $a0, ($t1)                      # otherwise load the byte at memory
@@ -51,7 +50,6 @@ print_numbers_loop:
             addu    $t1, $t1, 4                     # increment $t1 pointer by 4 bytes
             add     $t0, $t0, 1                     # i++
             b       print_numbers_loop
-
 end_print_numbers:
             li      $a0, 0xA                        # print a newline
             li      $v0, 11
@@ -61,13 +59,10 @@ end_print_numbers:
 
 bubble_sort:
             li      $t0, 1                          # i = 1
-
-bubble_sort_loop:
             beq     $t2, 1, end_bubble_sort         # if swapped = false, we're done.
             li      $t2, 1                          # swapped = false
-
-inner_loop:
-            beq     $t0, 20, end_inner_loop         # go through all elements
+bubble_sort_loop:
+            beq     $t0, 20, end_bubble_sort_loop   # go through all elements
             la      $t1, numbers                    # $t1 points to numbers arr
             
             move    $t3, $t0                        # $t3 = i-1
@@ -80,21 +75,19 @@ inner_loop:
             addu    $t1, $t1, 4
             lb      $t6, ($t1)                      # $t6 = numbers[i]
             
-            bgt     $t6, $t5, end_swap              # if numbers[i] > numbers[i-1]
-                                                    # don't swap
-swap:
+            bgt     $t6, $t5, end_swap              # if numbers[i] > numbers[i-1], skip.
+swap:       
             sw      $t5, ($t1)                      # numbers[i] = numbers[i-1]
             
             subu    $t1, $t1, 4                     
             sw      $t6, ($t1)                      # numbers[i-1] = numbers[i]
             
             li      $t2, 0                          # swapped = true
-
 end_swap:
             add     $t0, $t0, 1                     # i++
-            b       inner_loop
+            b       bubble_sort_loop
 
-end_inner_loop:
+end_bubble_sort_loop:
             b       bubble_sort            
             
 end_bubble_sort:
@@ -105,7 +98,6 @@ end_bubble_sort:
 exit:
             li $v0, 10
             syscall
-
 
             .data
 numbers:            .space 20
