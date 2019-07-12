4d217b24370f3c4e50e77fdb0fb8d53b8bfb1394
Jc <johnsjc@gmail.com>
2019-07-12 10:26:04 -0600

    update merge.s

--- mips/merge.s
+++ mips/merge.s
@@ -12,15 +12,17 @@ main:
             addu    $fp, $sp, 32
 
             la      $a0, list_a                         # first argument:  &list_a
-            li      $a1, 1                              # second argument: start_value
-            li      $a2, 2                              # third arument:   step
+            li      $a1, 0                              # second argument: start_value
+            li      $a2, 3                              # third argument:  step
+            li      $a3, 5                              # fourth argument: length
             jal     populate_list                       # list_a = [1, 3, 5, 7]
             move    $a0, $v0
             jal     print_list
 
             la      $a0, list_b                         # list_b = [2, 4, 6, 8]
-            li      $a1, 2
+            li      $a1, 3
             li      $a2, 2
+            li      $a3, 3
             jal     populate_list
             move    $a0, $v0
             jal     print_list
@@ -44,7 +46,8 @@ exit:
 # a0: &list
 # a1: start_value
 # a2: step
-# clobbers t0, t1, t2, t3
+# a3: length
+# clobbers t0, t1, t2, t3, t4
 populate_list:
             subu    $sp, $sp, 32
             sw      $fp, 28($sp)
@@ -55,11 +58,12 @@ populate_list:
             move    $t1, $a0                            # first parameter:  &list
             move    $t2, $a1                            # second parameter: start_value
             move    $t3, $a2                            # third parameter:  step
+            move    $t4, $a3                            # fourth parameter: length
 
             move    $v0, $a0                            # return &list
 
 populate_list_loop:
-            beq     $t0, 4, end_populate_list_loop
+            beq     $t0, $t4, end_populate_list_loop
 
             sw      $t2, ($t1)                          # list[i] = value
 
@@ -157,63 +161,146 @@ end_print_list_loop:
 ########### end print_list
 
 ########### merge
-# a0: &list_a
-# a1: &list_b
-# clobbers t0, t1, t2, t3
-# $t0 &list_a
-# $t1 &list_b
-# 
+# clobbers t0, t1, t2, t3, and t9
 merge:
 
 merge_prologue:
-            subu    $sp, $sp, 32
-            sw      $fp, 28($sp)
-            sw      $ra, 24($sp)
-            addu    $fp, $sp, 32
+            subu    $sp, $sp, 36
+            sw      $fp, 32($sp)
+            sw      $ra, 28($sp)
+            addu    $fp, $sp, 36
 
 merge_init:
-            move    $t0, $a0                                # &list_a
-            move    $t1, $a1                                # &list_b
-            li      $t2, 0                                  # i = 0 (list_a index)
-            li      $t3, 0                                  # j = 0 (list_b index)
+            sw      $a0, 24($sp)                            # local &list_a
+            sw      $a1, 20($sp)                            # local &list_b
 
-            sw      $t0, 20($sp)                            # save values from being clobbered
-            sw      $t1, 16($sp)                            # list_length uses t0, t1, t2
-            sw      $t2, 12($sp)
-            
-            jal     list_length                             # len(list_a)
-            move    $t4, $v0
-
-            move    $a0, $a1                                # len(list_b)
             jal     list_length
-            move    $t5, $v0
-
-            lw      $t2, 12($sp)                            # restore values from stack
-            lw      $t1, 16($sp)
-            lw      $t0, 20($sp)
-
-            # t0 = $list_a
-            # t1 = $list_b
-            # t2 = 0 (i)
-            # t3 = 0 (j)
-            # t4 = len(list_a)
-            # t5 = len(list_b)
-            # t6 and t7 will be temp variables
-
-            move    $v0, $t0                                # placeholder return &list_a 
+            sw      $v0, 16($sp)                            # local len(list_a)
             
+            move    $a0, $a1
+            jal     list_length                             # local len(list_b)
+            sw      $v0, 12($sp)
+
+            la      $t9, sorted                             # local &sorted
+            sw      $t9, 8($sp)
+
+            li      $t0, 0                                  # i = 0 (list_a index)
+            li      $t1, 0                                  # j = 0 (list_b index)
+
+            lw      $v0, 8($sp)                             # return sorted
+
+            # 24(sp) = &list_a
+            # 20(sp) = &list_b
+            # 16(sp) = len(list_a)
+            # 12(sp)  = len(list_b)
+            # 8(sp)  = &sorted
+            # t0 = list_a index (i)
+            # t1 = list_b index (j)
+            # t2 and t3 are list_a[i] and list_b[j]
+
+merge_loop:
+            lw      $t9, 16($sp)
+            beq     $t0, $t9, merge_copy_b_to_a             # copy rest of list_b
+                                                            # if list_a exhausted
+            lw      $t9, 12($sp)
+            beq     $t1, $t9, merge_copy_a_to_b             # copy rest of list_a            
+
+merge_compare:
+            lw      $t9, 24($sp)                            # t2 = list_a[i]          
+            lw      $t2, ($t9)
+
+            lw      $t9, 20($sp)                            # t3 = list_b[j]
+            lw      $t3, ($t9)
+
+            bgt     $t2, $t3, list_b_smaller                
+list_a_smaller:
+            lw      $t9, 8($sp)                             # sorted[k] = list_a[i]
+            sw      $t2, ($t9)
+
+            add     $t0, $t0, 1                             # i++
+
+            lw      $t9, 24($sp)                            # list_a++    
+            addu    $t9, $t9, 4
+            sw      $t9, 24($sp)
             
+            lw      $t9, 8($sp)
+            addu    $t9, $t9, 4                             # sorted++
+            sw      $t9, 8($sp)
+
+            b       merge_loop
+list_b_smaller:
+            lw      $t9, 8($sp)                             # sorted[k] = list_b[j]
+            sw      $t3, ($t9)
+
+            add     $t1, $t1, 1                             # j++
+
+            lw      $t9, 20($sp)                            # list_b++
+            addu    $t9, $t9, 4
+            sw      $t9, 20($sp)                         
+            
+            lw      $t9, 8($sp)
+            addu    $t9, $t9, 4                             # sorted++
+            sw      $t9, 8($sp)
+
+            b       merge_loop
+
+
+merge_copy_a_to_b:
+            lw      $t9, 16($sp)
+            beq     $t0, $t9, merge_loop_end
+            
+            lw      $t9, 24($sp)                            # t2 = list_a[i]
+            lw      $t2, ($t9)
+
+            lw      $t9, 8($sp)                             # t9 = sorted[k]
+            sw      $t2, ($t9)                              # sorted[k] = list_a[i]
+            
+            add     $t0, $t0, 1                             # i++
+
+            lw      $t9, 24($sp)                            # list_a++
+            addu    $t9, $t9, 4
+            sw      $t9, 24($sp)
+
+            lw      $t9, 8($sp)                             # sorted++
+            addu    $t9, $t9, 4
+            sw      $t9, 8($sp)
+
+            b       merge_copy_a_to_b
+merge_copy_b_to_a:
+            lw      $t9, 12($sp)                            
+            beq     $t1, $t9, merge_loop_end
+           
+            lw      $t9, 20($sp)                            # t3 = list_b[j]
+            lw      $t3, ($t9)
+            
+            lw      $t9, 8($sp)                             # t9 = sorted[k]
+            sw      $t3, ($t9)                              # sorted[k] = list_b[j]
+           
+            add     $t1, $t1, 1
+
+            lw      $t9, 20($sp)
+            addu    $t9, $t9, 4
+            sw      $t9, 20($sp)
+
+            lw      $t9, 8($sp)
+            addu    $t9, $t9, 4
+            sw      $t9, 8($sp)
+            
+            b       merge_copy_b_to_a
+
+merge_loop_end:
+                                                           # if list_b exhausted            
 merge_epilogue:
-            lw      $ra, 24($sp)
-            lw      $fp, 28($sp)
-            addu    $sp, $sp, 32 
+            lw      $ra, 28($sp)
+            lw      $fp, 32($sp)
+            addu    $sp, $sp, 36 
             jr      $ra
 
 
             .data
-list_a:             .space 16
+list_a:             .space 20
                     .word 0xFFFFFFFF
-list_b:             .space 16
+list_b:             .space 12
                     .word 0xFFFFFFFF
 sorted:             .space 32
                     .word 0xFFFFFFFF
